25e1533fc56d4d6f9638f533874cf9a9b1710c2c
Arthur Casey <57399280+xStar2222@users.noreply.github.com>
2022-10-27 17:42:05 -0400

    Add files via upload

---
+++ College App Thing.py
@@ -0,0 +1,28 @@
+
+Question = input("Enter College:")
+if Question == ("UCF"):
+    print("""
+          The Application Fee is: $35
+          ..........................................................................................................
+          The University of Central Florida is a public research university in unincorporated Orange County, Florida. 
+          It is part of the State University System of Florida. With 71,913 students as of the Fall 2020 semester, it 
+          currently has the largest student body of any public university in the United States. University of Central Florida 
+          admissions is more selective with an acceptance rate of 45%. Half the applicants admitted to UCF have an SAT score 
+          between 1160 and 1340 or an ACT score of 25 and 30.
+          """)
+if Question == ("UF"):
+    print("The Application Fee is: $30")
+if Question == ("FIT"):
+    print("The Application Fee is: $0")
+if Question == ("FSU"):
+    print("The Application Fee is: $30")
+if Question == ("FSF"):
+    print("The Application Fee is: $30")
+if Question == ("FIU"):
+    print("The Application Fee is: $30")
+if Question == ("RC"):
+    print("The Application Fee is: $50")
+if Question == ("NSU"):
+    print("The Application Fee is: $50")
+if Question == ("USF"):
+    print("The Application Fee is: $50")
